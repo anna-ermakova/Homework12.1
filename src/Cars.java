a15c88043b6c39c3427292cb90e087fbedc02173
anna-ermakova <korovanna8@mail.ru>
2022-11-15 17:26:43 +0300

    ООП введение. ДЗ /Cars/

--- src/Cars.java
+++ src/Cars.java
@@ -8,11 +8,29 @@ public class Cars {
     private String country;
 
     public Cars(String brand, String model, double engineVolume, String color, int year, String country) {
+        if (brand=="" || brand==null) {
+            brand = "default";
+        }
         this.brand = brand;
+        if (model=="" || model==null) {
+            model = "default";
+        }
         this.model = model;
+        if (engineVolume<=0) {
+            engineVolume = 1.5;
+        }
         this.engineVolume = engineVolume;
+        if (color=="" || color==null) {
+            color = "белый";
+        }
         this.color = color;
+        if (year<=0) {
+            year = 2000;
+        }
         this.year = year;
+        if (country=="" || country==null) {
+            country = "default";
+        }
         this.country = country;
     }
 
